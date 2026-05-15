739fda22ea7086d4892a199c2f1f6497867bf91e
Miguel <temporadamike@gmail.com>
2026-05-14 18:26:19 -0600

    Codigo inicial

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <HelloWorld />
+  <login />
 </template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/login.vue';
 </script>
